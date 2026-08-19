60c76df4432abe2e2907c51134abdc2c29379a22
Ziyan Nafis Assaify <assaify256@gmail.com>
2026-08-19 09:13:49 +0700

    Remove back end

--- back-end/src/routes/v2-bff/admin-role/routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import adminHomeRouter from "./home.routes.ts"
-
-const adminRouter = express.Router()
-
-adminRouter.use("/home", adminHomeRouter)
-
-export default adminRouter
--- back-end/src/utils/async-handler.ts
+++
@@ -1,9 +0,0 @@
-import type { NextFunction, Request, RequestHandler, Response } from "express";
-
-export const asyncHandler = (
-    fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>,
-): RequestHandler => {
-    return (req, res, next) => {
-        void fn(req, res, next).catch(next);
-    };
-};
