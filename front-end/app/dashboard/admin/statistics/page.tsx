6a5fbe47ec53032a30fa9bc96c6f9d7cbebe86cd
Ziyan Nafis Assaify <assaify256@gmail.com>
2026-08-22 14:24:59 +0700

    Responsive dashboard work

---
+++ front-end/app/dashboard/admin/statistics/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Page() {
+    return (
+        <div className="flex flex-col p-8">
+            <h1 className="text-3xl">Payment Management</h1>
+            <p>Track and manage clinic payments</p>
+            <div className="flex flex-row mt-8"></div>
+        </div>
+    );
+}
--- front-end/custom-components/cards/custom-card.tsx
+++ front-end/custom-components/cards/custom-card.tsx
@@ -9,7 +9,7 @@ export default function CustomCard({
     className = ""
 }) {
     return (
-        <Card className={`flex-1 my-4 mx-2 shadow-2xl ${className}`}>
+        <Card className={`shadow-2xl ${className}`}>
             <CardContent className="flex flex-row justify-between items-center">
                 <div className="flex flex-col">
                     <h2 className="text-md mb-4 text-gray-500">{title}</h2>
