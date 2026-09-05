0d9fcc925ebcfb56ee83915b12afcb0e80f6c3bb
assaify256 <112068372+assaify256@users.noreply.github.com>
2026-09-05 11:35:17 +0700

    Revert "Chart fix and some styling fix"
    This reverts commit 6bdd2587d0a12435a10a852d87287b5ede73cf95.

--- front-end/app/dashboard/admin/statistics/appointment-status.chart.tsx
+++ front-end/app/dashboard/admin/statistics/appointment-status.chart.tsx
@@ -3,10 +3,6 @@
 import { ChartConfig, ChartContainer } from "@/components/ui/chart";
 import {
     Cell,
-    DefaultLegendContent,
-    DefaultLegendContentProps,
-    Legend,
-    LegendType,
     Line,
     LineChart,
     Pie,
@@ -42,31 +38,15 @@ const chartData = [
     },
 ];
 
-const MyCustomPie = (props: PieSectorShapeProps) => (
-    <Sector {...props} fill={colors[props.index % colors.length]} />
-);
+const MyCustomPie = (props: PieSectorShapeProps) => <Sector {...props} fill={colors[props.index % colors.length]} />;
 
 export default function AppointmentStatus() {
     return (
         <ChartContainer config={chartConfig}>
             <PieChart data={chartData}>
-                <Pie dataKey={"value"} shape={MyCustomPie} label={true}></Pie>
-                <Legend
-                    content={({
-                        payload,
-                        ...rest
-                    }: DefaultLegendContentProps) => {
-                        const newPayload = payload?.map((item, index) => {
-                            return { ...item, color: colors[index] };
-                        });
-                        return (
-                            <DefaultLegendContent
-                                payload={newPayload}
-                                {...rest}
-                            />
-                        );
-                    }}
-                ></Legend>
+                <Pie dataKey={"value"} shape={MyCustomPie}>
+
+                </Pie>
                 <Tooltip />
             </PieChart>
         </ChartContainer>
