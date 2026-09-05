3bb2f384e5988d80b15d3e27201f5b1549c9e899
assaify256 <112068372+assaify256@users.noreply.github.com>
2026-09-05 11:35:17 +0700

    Revert "Auth rework and front page"
    This reverts commit 85a61d01477250813f4db9099f89c77d910f30b4.

--- front-end/app/(auth)/sign-in/page.tsx
+++ front-end/app/(auth)/sign-in/page.tsx
@@ -1,40 +1,25 @@
-"use client";
+"use client"
 //Components
-import { SignInForm } from "@/custom-components/form/sign-in.form";
+
+import Image from "next/image";
+import { LoginForm } from "@/custom-components-old/general/login-form";
 
 // Assets
-import { useRouter } from "next/navigation";
+import Logo from "@/public/logo.png";
 
 export default function LoginPage() {
-    const router = useRouter();
-    fetch("http://localhost:8080/api/v1/auth/me", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        credentials: "include",
-    })
-        .then((response) => {
-            if (!response.ok) {
-                return;
-            }
-            return response.json();
-        })
-        .then((resObj) => {
-            if (resObj) {
-                resObj.data.role &&
-                    router.push(`/dashboard/${resObj.data.role}`);
-            }
-        });
-
     return (
-        <div className="flex flex-col gap-4 md:p-6 h-screen">
+        <div className="flex flex-col gap-4 p-4 md:p-6 h-screen">
+            <div className="flex justify-center gap-2 md:justify-start">
+                <a href="#" className="flex items-center gap-2 font-medium">
+                    <div className="flex size-24 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                        <Image alt="App Logo" src={Logo} loading="eager"/>
+                    </div>
+                </a>
+            </div>
             <div className="flex flex-1 items-center justify-center">
-                <div className="flex flex-col w-1/3 pb-8">
-                    <SignInForm
-                        className="border-2 border-black bg-mauve-50"
-                        signUpLink="/sign-up"
-                    />
+                <div className="w-full max-w-xs">
+                    <LoginForm signUpLink="/sign-up" />
                 </div>
             </div>
         </div>
