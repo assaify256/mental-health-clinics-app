c92ec669f8166dd0cbdf43e6b4e8b55c42e24683
assaify256 <112068372+assaify256@users.noreply.github.com>
2026-09-05 11:35:17 +0700

    Revert "Work"
    This reverts commit 751bcb5b7d0aa850d88f017d4324ba0b03517848.

--- front-end/app/dashboard/client/(home)/page.tsx
+++ front-end/app/dashboard/client/(home)/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import CustomCard from "@/custom-components/cards/custom-card";
 import Stack from "@/custom-components/page/stack";
@@ -11,15 +10,7 @@ import {
     mainDivClass,
     mainTitleClass,
 } from "@/styles/classNames.admin";
-import {
-    Calendar,
-    CalendarCheck,
-    CalendarClock,
-    ClipboardCheck,
-    Notebook,
-    NotepadText,
-    Pill,
-} from "lucide-react";
+import { CalendarCheck, CalendarClock, NotepadText, Pill } from "lucide-react";
 
 const tableHeaders = [
     {
@@ -77,32 +68,6 @@ export default function Page() {
                     icon={<NotepadText />}
                     number={0}
                 />
-            </Stack>
-            <Stack>
-                <Card className="shadow-2xl flex-1 m-2">
-                    <CardContent className="flex flex-col">
-                        <h2 className="text-lg font-semibold">Quick Actions</h2>
-                        <Button className="m-0.5 justify-baseline">
-                            <Calendar />
-                            Book Appointments
-                        </Button>
-                        <Button className="m-0.5 justify-baseline">
-                            <Notebook />
-                            Write Notes
-                        </Button>
-                        <Button className="m-0.5 justify-baseline">
-                            <ClipboardCheck />
-                            View Results
-                        </Button>
-                    </CardContent>
-                </Card>
-                <Card className="shadow-2xl flex-2 m-2">
-                    <CardContent>
-                        <h2 className="text-lg font-semibold">
-                            Upcoming Appointments
-                        </h2>
-                    </CardContent>
-                </Card>
             </Stack>
             <Stack>
                 <Card className={`shadow-2xl flex-1 m-2`}>
--- front-end/custom-components/cards/custom-card.tsx
+++ front-end/custom-components/cards/custom-card.tsx
@@ -1,26 +1,13 @@
 import { House } from "lucide-react";
 import { Card, CardContent } from "../../components/ui/card";
 
-export default async function CustomCard({
+export default function CustomCard({
     title = "Title",
     icon = <House />,
     number = 0,
     preNumber = "",
-    className = "",
-    endpoint = "/",
+    className = ""
 }) {
-    
-    try {
-        const response = await fetch(`http://localhost:8080${endpoint}`, {
-        credentials: "include",
-        method: "GET",
-    });
-    const data = await response.json()
-    number = data
-    } catch (error) {
-        console.log(error)
-    }
-    
     return (
         <Card className={`shadow-2xl ${className}`}>
             <CardContent className="flex flex-row justify-between items-center">
